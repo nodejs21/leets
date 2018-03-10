d29fb584677832b840efb202f78677ad9eb83387
unknown <nodejs21@gmail.com>
2018-03-11 00:46:03 +0500

    again

--- db/mongoose.js
+++ db/mongoose.js
@@ -5,8 +5,11 @@ mongoose.Promise = global.Promise;
 // admin:root@ds139585.mlab.com:39585/db_todos
 
 // mongoose.connect('mongodb://localhost:27017/db_leets');
-async () => {
-	await mongoose.connect('mongodb://admin:root@ds211309.mlab.com:11309/db_leets');
-};
+try {
+	mongoose.connect('mongodb://admin:root@ds211309.mlab.com:11309/db_leets');
+} catch(e) {
+	console.log(e);
+}
+
 
 module.exports = {mongoose};
