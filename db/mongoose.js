1a4003a068b12f37e4a60f5f4c28e9905c5fcd46
unknown <nodejs21@gmail.com>
2018-03-11 00:01:11 +0500

    setup-05

--- db/mongoose.js
+++ db/mongoose.js
@@ -4,6 +4,7 @@ mongoose.Promise = global.Promise;
 
 // admin:root@ds139585.mlab.com:39585/db_todos
 
-mongoose.connect('mongodb://localhost:27017/db_leets');
+// mongoose.connect('mongodb://localhost:27017/db_leets');
+mongoose.connect('mongodb://admin:root@ds139585.mlab.com:39585/db_leets');
 
 module.exports = {mongoose};
