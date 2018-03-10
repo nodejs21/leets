862b77ddfea8229bb6f513dbbbd6326e25fd934d
unknown <nodejs21@gmail.com>
2018-03-11 00:55:40 +0500

    äsdfgshf

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,12 +1,14 @@
 const mongoose = require('mongoose');
 
-// mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise;
 
 // admin:root@ds139585.mlab.com:39585/db_todos
 
 // mongoose.connect('mongodb://localhost:27017/db_leets');
 try {
-	mongoose.connect('mongodb://admin:root@ds211309.mlab.com:11309/db_leets');
+	async function hio() {
+		await mongoose.connect('mongodb://admin:root@ds211309.mlab.com:11309/db_leets');
+	}
 } catch(e) {
 	console.log(e);
 }
