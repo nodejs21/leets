a54ba55ed44b31603451e6ef170349a47c315b46
unknown <nodejs21@gmail.com>
2018-03-11 00:42:25 +0500

    sadf

--- db/mongoose.js
+++ db/mongoose.js
@@ -5,6 +5,8 @@ mongoose.Promise = global.Promise;
 // admin:root@ds139585.mlab.com:39585/db_todos
 
 // mongoose.connect('mongodb://localhost:27017/db_leets');
-mongoose.connect('mongodb://admin:root@ds211309.mlab.com:11309/db_leets?authSource=admin');
+async () => {
+	await mongoose.connect('mongodb://admin:root@ds211309.mlab.com:11309/db_leets');
+};
 
 module.exports = {mongoose};
