fc47b834f9f27142117314b90717469a8da340b7
unknown <nodejs21@gmail.com>
2018-03-11 00:24:45 +0500

    unhandeled promise rejection handeled

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,6 @@ var router = express.Router();
 
 router.get('/', function (req, res, next) {
 	
-	// var XHR = new XMLHttpRequest();
-	// XHR.open("GET", "https://www.espn.in/cricket/series/8038/report/1133013/afghanistan-vs-nepal-16th-match-group-b-world-cup-qualifier", true);
-	// XHR.onreadystatechange = function(){
-	// 	if(XHR.readyState == 4){
-	// 		console.log(XHR.responseText);
-	// 	}
-	// }
-	// XHR.send();
-
-	// res.end("Done");
 });
 
 module.exports = router;
