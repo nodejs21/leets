41dc9adb4c4e189ab7c3a1eaba98f5492a59d367
unknown <nodejs21@gmail.com>
2018-03-11 00:59:55 +0500

    erterhedhfsfa

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,17 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;
 
 // admin:root@ds139585.mlab.com:39585/db_todos
 
 // mongoose.connect('mongodb://localhost:27017/db_leets');
-try {
-	async function hio() {
-		await mongoose.connect('mongodb://admin:root@ds211309.mlab.com:11309/db_leets');
-	}
-} catch(e) {
-	console.log(e);
-}
+mongoose.connect('mongodb://admin:root@ds211309.mlab.com:11309/db_leets');
 
 
 module.exports = {mongoose};
