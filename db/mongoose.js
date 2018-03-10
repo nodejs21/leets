49310194b8c1ee0467c9ef9f60db84e624da8552
unknown <nodejs21@gmail.com>
2018-03-11 00:37:00 +0500

    ?authSource added in mongourl

--- db/mongoose.js
+++ db/mongoose.js
@@ -5,11 +5,6 @@ mongoose.Promise = global.Promise;
 // admin:root@ds139585.mlab.com:39585/db_todos
 
 // mongoose.connect('mongodb://localhost:27017/db_leets');
-mongoose.connect('mongodb://admin:root@ds211309.mlab.com:11309/db_leets')
-.then(() => {
-
-}, (err) => {
-	console.log(err);
-});
+mongoose.connect('mongodb://admin:root@ds211309.mlab.com:11309/db_leets?authSource=admin');
 
 module.exports = {mongoose};
